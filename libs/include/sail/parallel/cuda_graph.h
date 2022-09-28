f2648c221ced86b7742588e03341d9b74e7a7826
sailing-innocent <sailing-innocent@foxmail.com>
2022-09-28 22:28:18 +0800

    update 2022-09-28

---
+++ libs/include/sail/parallel/cuda_graph.h
@@ -0,0 +1,18 @@
+/**
+ * @file cuda_graph.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-09-28
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#pragma once
+
+// common
+// cuda.h
+// deque
+// functional
+
---
+++ libs/include/sail/parallel/nn/losses/l1.h
@@ -0,0 +1,18 @@
+/**
+ * @file l1.h
+ * @author sailing-innocent (sailing-innocent@foxmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-09-28
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+ #pragma once 
+
+ //common
+ //gpu-matrix
+ //common-device
+ //loss
+ 
