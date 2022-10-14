e6ab638796e71919e88519603d0e0a68fb9e9635
sailing-innocent <sailing-innocent@foxmail.com>
2022-10-14 21:35:17 +0800

    refactor(main): to a mature framework

---
+++ libs/sail/include/sail/hello.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef HELLO_H_
+#define HELLO_H_
+
+#include "common.h"
+#include <iostream>
+#include <string>
+
+SAIL_NAMESPACE_BEGIN
+std::string hello();
+
+SAIL_NAMESPACE_END
+
+#endif
--- libs/sail/utest/hello_test.cc
+++ libs/sail/utest/hello_test.cc
@@ -1,6 +1,9 @@
 #include <gtest/gtest.h>
+#include "sail/hello.h"
 
 TEST(HelloTest, BasicAssertions) {
     EXPECT_STRNE("hello", "world");
     EXPECT_EQ(7 * 6, 42);
+    std::string s = sail::hello();
+    EXPECT_STREQ(s.c_str(), "Hello");
 }
