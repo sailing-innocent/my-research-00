fdbc47aad8d9dd86e66fec1f64104613144ba852
sailing-innocent <sailing-innocent@foxmail.com>
2022-10-28 23:20:58 +0800

    feat: add controlable scene app

--- scripts/compile_shaders.ps1
+++ scripts/compile_shaders.ps1
@@ -1,5 +1,6 @@
 $basePath = "E:/assets/shaders"
-$folderName = "canvas"
+# $folderName = "canvas"
+$folderName = "scene"
 
 glslc "${basePath}/${folderName}/shader.vert" -o "${basePath}/${folderName}/vert.spv"
 glslc "${basePath}/${folderName}/shader.frag" -o "${basePath}/${folderName}/frag.spv"
